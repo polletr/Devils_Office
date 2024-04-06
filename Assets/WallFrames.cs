657b841cdb5c1bf465fd8273c1a40c9dbbce6b37
Renee Camillo Pollet <r.camillopollet798@mybvc.ca>
2024-04-06 13:49:17 -0600

    Frames on Wall generated randomly

---
+++ Assets/WallFrames.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallFrames : BaseObject
+{
+    [SerializeField]
+    List<GameObject> frames = new List<GameObject>();
+    [SerializeField]
+    List<GameObject> images = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        foreach (var frame in frames)
+        {
+            int randomCheck = Random.Range(0, 10);
+
+            if (randomCheck >= 8)
+            {
+                int random = Random.Range(0, images.Count);
+                Instantiate(images[random], frame.transform);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
