a7baa995382dbba093f693f997473cc234ea5bd2
Daril Camilo <darilvendas@hotmail.com>
2024-04-05 19:12:26 -0600

    Added Ember Particles

---
+++ Assets/Floating Embers/Demo Scene/Script/Demo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+    public GameObject ojbWindzone;
+    public GameObject[] prefabs;
+
+    public void SwitchWindzone(bool selected)
+    {
+        ojbWindzone.SetActive(selected);
+    }
+
+    public void dropDown_Click(int index)
+    {
+        for (int i = 0; i < prefabs.Length; i++)
+        {
+            prefabs[i].SetActive(false);
+        }
+
+        prefabs[index].SetActive(true);
+    }
+}
---
+++ Assets/Floating Embers/ReadMe.txt
@@ -0,0 +1,13 @@
+-------------------
+Floating Embers
+-------------------
+
+Floating Embers is a collection of embers particle effects. You can easily use them to add atmosphere to your environment. You can also use them as menu backgrounds for action/war/RPG games.
+
+Contains:
+- 22 ember shapes. 
+- 1 demo scene.
+- 14 embers prefabs
+- 7 Textures.
+
+WindZone is supported. You may use the WindZone's direction and strength to control the direction of embers' movement.
